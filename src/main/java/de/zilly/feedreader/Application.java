4e57bd2df45b90408ff6a90d5ee0c208d22ff870
azilly-de <azilly-de@users.noreply.github.com>
2025-03-03 15:45:46 +0100

    chore: Rename Java package

--- src/main/java/de/zilly/feedreader/Application.java
+++ src/main/java/de/zilly/feedreader/Application.java
@@ -1,4 +1,4 @@
-package org.vaadin.example;
+package de.zilly.feedreader;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
--- src/main/java/de/zilly/feedreader/GreetService.java
+++ src/main/java/de/zilly/feedreader/GreetService.java
@@ -1,4 +1,4 @@
-package org.vaadin.example;
+package de.zilly.feedreader;
 
 import java.io.Serializable;
 import org.springframework.stereotype.Service;
